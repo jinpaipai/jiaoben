b6978a651bf36bab5ee526617f9753eba78e9011
jinpaipai <85398465+jinpaipai@users.noreply.github.com>
2025-08-29 15:56:19 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -26,6 +26,8 @@ FILES_TO_BACKUP=(
     "/opt/1panel/apps"
     "/opt/1panel/www"
     "/etc/localtime"
+    "$HOME/.config/qBittorrent"
+    "$HOME/.local/share/qBittorrent"
     "/etc/systemd/system/nezha-dashboard.service"
     "/etc/systemd/system/nezha-agent.service"
 )
